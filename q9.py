1cba653840bb1c817b665acd631299e640b13abb
Harish S.G <harishsg99@gmail.com>
2021-04-17 21:15:41 +0530

    Create q9.py

---
+++ q9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+    # Store the number in a variable
+        number = x
+    # This will store the reverse of the number
+        reverse = 0
+        while number:
+            reverse = reverse * 10 + number % 10
+            number //= 10
+        return x == reverse
