ac0e4af96f2a8a6e0e6a5c1a07c23a3ff6547d5b
Harish S.G <harishsg99@gmail.com>
2021-04-18 13:39:08 +0530

    Create q101.py

---
+++ q101.py
@@ -0,0 +1,22 @@
+class Solution:
+  def isSymmetric(self, root):
+    if root is None:
+      return True
+
+    stack = [[root.left, root.right]]
+
+    while len(stack) > 0:
+      pair = stack.pop()
+      left = pair[0]
+      right = pair[1]
+
+      if left is None and right is None:
+        continue
+      if left is None or right is None:
+        return False
+      if left.val == right.val:
+        stack.append([left.left, right.right])
+        stack.append([left.right, right.left])
+      else:
+        return False
+    return True
